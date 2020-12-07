f50a78adfb2a340faf13310a5e37e8c02b3a1dd1
j3ffh95 <j3ffh95@gmail.com>
2020-12-07 16:40:42 -0500

    Made the active class switch between links

--- main.js
+++ main.js
@@ -10,6 +10,17 @@ const openNav = (event) => {
     menuWrap.classList.remove("showNav");
     menuToggle.classList.remove("is-clicked");
   }
+  // Check to see if the element clicked has an atrribute of href
+  if (event.target.matches("[href]")) {
+    let activeChild = event.target;
+    activeChild.classList.add("active");
+    Array.from(activeChild.parentElement.children).forEach((child) => {
+      if (child !== activeChild) {
+        child.classList.remove("active");
+      }
+    });
+  }
+  // console.log(event.target);
 };
 
 document.addEventListener("click", openNav);
