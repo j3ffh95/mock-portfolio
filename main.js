446582613056ec8b3556a8645159deae0e3f0761
j3ffh95 <j3ffh95@gmail.com>
2020-12-07 14:43:12 -0500

    Fixed animation on click

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ let menuToggle = document.querySelector(".menu-toggle");
 let menuWrap = document.querySelector(".main-nav-wrap");
 
 const openNav = (event) => {
-  if (menuWrap.style.display === "none") {
-    menuWrap.style.display = "block";
-  } else {
-    menuWrap.style.display = "none";
-  }
+  menuWrap.classList.toggle("showNav");
   menuToggle.classList.toggle("is-clicked");
 };
+
 menuToggle.addEventListener("click", openNav);
