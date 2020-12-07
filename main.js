80c0ff51619da41c91c0b6f29adbe4d77e3031ee
j3ffh95 <j3ffh95@gmail.com>
2020-12-07 16:13:26 -0500

    Added a bit of JS Logic to navbar

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ let menuToggle = document.querySelector(".menu-toggle");
 let menuWrap = document.querySelector(".main-nav-wrap");
 
 const openNav = (event) => {
-  menuWrap.classList.toggle("showNav");
-  menuToggle.classList.toggle("is-clicked");
+  let cList = event.target.classList;
+  if (cList.contains("menu-toggle") || cList.contains("menu-hamburger")) {
+    menuWrap.classList.toggle("showNav");
+    menuToggle.classList.toggle("is-clicked");
+  } else {
+    menuWrap.classList.remove("showNav");
+    menuToggle.classList.remove("is-clicked");
+  }
 };
 
-menuToggle.addEventListener("click", openNav);
+document.addEventListener("click", openNav);
